5f7bc204641c447827a11034136583c7a89e85f0
PetrenkoS <petrenko.s@yahoo.com>
2023-04-07 22:35:19 -0400

    Refactoring Actions Test class

--- src/test/java/ActionsTests.java
+++ src/test/java/ActionsTests.java
@@ -9,13 +9,19 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.BasePage;
 import pages.LoginPage;
+import pages.PlaylistsPage;
+import pages.SongsPage;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ActionsTests extends BaseTest {
 
     LoginPage loginPage = new LoginPage();
     BasePage basePage = new BasePage();
+    PlaylistsPage playlistsPage = new PlaylistsPage();
+
+    SongsPage songsPage = new SongsPage();
 
 
     @Test
@@ -23,11 +29,11 @@ public class ActionsTests extends BaseTest {
         PageFactory.initElements(getDriver(), loginPage);
         // hover over in clickPlayBtn
         loginPage.login("demo@class.com", "te$t$tudent");
-        clickPlayBtn();
-        Assert.assertTrue(pauseBtnExists());
+        playlistsPage.clickPlayBtn();
+        Assert.assertTrue(playlistsPage.pauseBtnExists());
 
         // Comparing numbers of elements example
-        List<WebElement> songs = basePage.getDriver().findElements(By.cssSelector("[data-test='song-card']"));
+        List<WebElement> songs = songsPage.getSongs();
 
         int songsNumberBefore = songs.size();
         System.out.println(songsNumberBefore);
@@ -44,15 +50,22 @@ public class ActionsTests extends BaseTest {
     }
 
     @Test
-    public void renamePlaylist() {
+    public void renamePlaylist() throws InterruptedException {
         PageFactory.initElements(getDriver(), loginPage);
         // double click
         String playlistName = "Summer songs";
 
         loginPage.login("demo@class.com", "te$t$tudent");
-        doubleClickChoosePlaylist();
-        enterPlaylistName(playlistName);
-        String newName = getPlaylistName();
+        String oldName = playlistsPage.getPlaylistName();
+        if(oldName.equals(playlistName)){
+            playlistName = "Winter songs";
+        }
+        System.out.println(playlistName);
+        playlistsPage.doubleClickChoosePlaylist();
+        playlistsPage.enterPlaylistName(playlistName);
+        Thread.sleep(1000);
+        String newName = playlistsPage.getPlaylistName();
+        System.out.println(newName);
         Assert.assertEquals(playlistName, newName);
     }
 
@@ -61,7 +74,7 @@ public class ActionsTests extends BaseTest {
         PageFactory.initElements(getDriver(), loginPage);
         // right click
         loginPage.login("demo@class.com", "te$t$tudent");
-        goToAllSongs();
+        playlistsPage.goToAllSongs();
         WebElement firstSong = basePage.getDriver().findElement(By.cssSelector(".song-item"));
         Actions actions = new Actions(basePage.getDriver());
         actions.contextClick(firstSong).perform();
@@ -71,42 +84,6 @@ public class ActionsTests extends BaseTest {
         basePage.waitUntilVisible(By.cssSelector("[data-testid='sound-bar-play']"));
     }
 
-    public void clickPlayBtn() {
-        Actions action = new Actions(basePage.getDriver());
-        WebElement playBtn = basePage.getDriver().findElement(By.cssSelector("[data-testid='play-btn']"));
-        action
-                .moveToElement(playBtn)
-                .perform();
-        playBtn.click();
-    }
-
-    public boolean pauseBtnExists() {
-        return basePage.getDriver().findElement(By.cssSelector("[data-testid='pause-btn']")).isDisplayed();
-    }
-
-
-    private void goToAllSongs() {
-        basePage.waitUntilClickable(By.cssSelector(".songs")).click();
-    }
-
-
-    public void doubleClickChoosePlaylist() {
-        WebElement playlistElement = basePage.waitUntilClickable(By.cssSelector(".playlist:nth-child(3)"));
-        Actions actions = new Actions(basePage.getDriver());
-        actions.doubleClick(playlistElement).perform();
-    }
-
-    public void enterPlaylistName(String name) {
-        WebElement playlistInputField = basePage.getDriver().findElement(By.cssSelector("input[name='name']"));
-        playlistInputField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), name);
-        playlistInputField.sendKeys(Keys.ENTER);
-    }
-
-    private String getPlaylistName() {
-        WebElement playlistElement = basePage.waitUntilVisible(By.cssSelector(".playlist:nth-child(3)>a"));
-        String name = playlistElement.getText();
-        return name;
-    }
 
     @Test
     public void countSongsInPlaylist() {
@@ -117,7 +94,7 @@ public class ActionsTests extends BaseTest {
         List<WebElement> songs = basePage.getDriver().findElements(By.cssSelector("#playlistWrapper .song-item"));
         int number = songs.size();
       //  Assert.assertEquals(number, 4); // can fail, depends on current number. This is just an example
-        
+
     }
 
 }
